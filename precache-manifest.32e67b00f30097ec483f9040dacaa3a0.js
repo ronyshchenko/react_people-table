ea15e33a50dfd0b1f3c1a714724a38550883a566
ronyshchenko <roma.onyshchenko@gmail.com>
2020-09-30 22:41:57 +0300

    Updates

---
+++ precache-manifest.32e67b00f30097ec483f9040dacaa3a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5cc3059f32d71010960a4b103b8db7",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "ac6e785d94a2eae34b08",
+    "url": "/react_people-table/static/css/main.a64938dd.chunk.css"
+  },
+  {
+    "revision": "06e204367514ded31467",
+    "url": "/react_people-table/static/js/2.a18ea63a.chunk.js"
+  },
+  {
+    "revision": "ac6e785d94a2eae34b08",
+    "url": "/react_people-table/static/js/main.f0b6ccee.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
